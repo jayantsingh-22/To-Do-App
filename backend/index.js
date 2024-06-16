b0d36b025a3d8fb96990c7cde325ef9de067f46f
jayantsingh-22 <jayantsingh640@gmail.com>
2024-06-16 16:58:24 +0530

    chore:added env variable to port

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const tasksRoutes = require('./routes/tasksRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const tasksRoutes = require("./routes/tasksRoutes");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/tasks', tasksRoutes);
+app.use("/tasks", tasksRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
